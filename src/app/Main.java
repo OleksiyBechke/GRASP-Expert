f758723edbdb6dfe57350738c851cf4a853845a3
Oleksiy Bechke <bechke@dlit.dp.ua>
2024-09-20 15:36:02 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Oleksiy");
+
+        Address address = new Address("Dnipro", "Slobozhanskyi", "100", "50505");
+
+        user.setAddress(address);
+
+        System.out.println(user.getAddress().toString());
+    }
+}
